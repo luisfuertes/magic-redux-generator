ba632ad446f8297405844918c5e645f577770188
Mobile 3 <luis.fuertes@clouddistrict.com>
2016-12-07 13:34:03 +0100

    Index fixed

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import {createTypes, createReducer, createActions} from './reduxGenerator';
-import webservices from './webservices';
-
+//import webservices from './webservices';
+var webservices       = require('./webservices');
 
 module.exports = {
   createTypes:createTypes,
