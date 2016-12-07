d560ec0aa7a9e50f305d5aa831500e4e91146f9b
Mobile 3 <luis.fuertes@clouddistrict.com>
2016-12-07 12:46:06 +0100

    index fixed

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 var reduxGenerator = require('./reduxGenerator');
 var webservices    = require('./webservices');
 
-
 module.exports = {
   reduxGenerator: reduxGenerator,
   webservices: webservices,
 };
-
-for (var c in constants) {
-  module.exports[c] = constants[c];
-}
